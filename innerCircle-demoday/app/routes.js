ce49599c0aa81ca64be4d0912df7c6bb07934a0c
Jada-Cherie <jada.c.townsend@gmail.com>
2023-05-22 17:03:34 -0400

    getting help with eotion to text API, commiting every change in routes.js and main. js

--- innerCircle-demoday/app/routes.js
+++ innerCircle-demoday/app/routes.js
@@ -1,6 +1,19 @@
 const ObjectId = require('mongodb').ObjectId
 // const { analyzeEmotions } = require('./utils');
 module.exports = function(app, passport, db,) {
+  // list of emotion keywords
+const emotionKeywords = {
+  // define a list of emotions and associated keywords
+  // e.g., 'sad' is associated with keywords like 'sad', 'unhappy', 'depressed', etc.
+  // the same is done for other emotions
+  //started with writing down 15 words for each and then used chatgpt to expand on the assocaiated words (about 50 words in each array)
+  sad: ['sad', 'unhappy', 'depressed', 'miserable', 'gloomy', 'heartbroken', 'tearful', 'downcast', 'despair', 'sorrowful', 'blue', 'melancholy', 'desolate', 'lonely', 'weepy', 'disappointed', 'devastated', 'troubled', 'hurt', 'painful', 'grief', 'wretched', 'forlorn', 'regretful', 'angst', 'bereaved', 'somber', 'dejected', 'lamenting', 'pessimistic', 'despondent', 'downhearted', 'heavy-hearted', 'morose', 'sullen', 'grieved', 'distressed', 'brokenhearted', 'woeful', 'pathetic', 'dismal', 'unfortunate', 'down', 'mournful', 'weeping', 'glum', 'tragic', 'dreary', 'upset', 'inconsolable'],
+  happy: ['happy', 'joyful', 'excited', 'delighted', 'gleeful', 'content', 'thrilled', 'cheerful', 'joyous', 'ecstatic', 'upbeat', 'elated', 'jubilant', 'smiling', 'grateful', 'satisfied', 'blissful', 'euphoric', 'overjoyed', 'radiant', 'pride', 'prideful', 'pleased', 'merry', 'mirthful', 'gratified', 'fulfilled', 'exhilarated', 'cheery', 'beaming', 'glad', 'buoyant', 'exultant', 'animated', 'uplifted', 'festive', 'jovial', 'sunny', 'triumphant', 'delirious', 'ecstatic', 'triumphal', 'enthusiastic', 'bouncy', 'jolly', 'vibrant', 'rejoicing', 'glee', 'carefree', 'satisfied'],
+  excited: ['excited', 'thrilled', 'eager', 'enthusiastic', 'animated', 'pumped', 'energized', 'passionate', 'fired up', 'exhilarated', 'amped', 'raring', 'frenzied', 'electric', 'dynamic', 'enthralling', 'intense', 'vibrant', 'rousing', 'roused', 'enthusiasm', 'anticipation', 'ardent', 'zealous', 'spirited', 'fervent', 'ecstatic', 'lively', 'overwhelmed', 'enthralling', 'rapturous', 'thrilling', 'exultant', 'delirious', 'passion', 'fanatical', 'wild', 'exuberant', 'vigorous', 'elevated', 'jubilant', 'delighted', 'triumphant', 'fired', 'dynamic', 'animated', 'blazing', 'enlivened', 'vivacious', 'passionate', 'bubbling', 'electrified'],
+  angry: ['angry', 'frustrated', 'irritated', 'furious', 'enraged', 'infuriated', 'livid', 'outraged', 'incensed', 'aggravated', 'annoyed', 'resentful', 'hostile', 'irate', 'tempestuous', 'provoked', 'exasperated', 'mad', 'fuming', 'indignant', 'rage', 'wrath', 'ire', 'vexed', 'infuriate', 'agitated', 'boiling', 'irascible', 'angry', 'cross', 'offended', 'miffed', 'bitter', 'displeased', 'hateful', 'resentful', 'sore', 'worked up', 'incensed', 'provoked', 'riled', 'sulky', 'splenetic', 'outraged', 'enraged', 'spiteful', 'livid', 'outright', 'furious', 'angry', 'infuriating'],
+  fear: ['fear', 'scared', 'anxious', 'afraid', 'terrified', 'nervous', 'panicked', 'startled', 'horrified', 'dreadful', 'petrified', 'worried', 'uneasy', 'tense', 'phobic', 'paranoid', 'frightened', 'apprehensive', 'fearful', 'anxiously', 'terror', 'dread', 'panic', 'horror', 'unease', 'distress', 'shock', 'anxiety', 'trepidation', 'terror-stricken', 'jittery', 'trembling', 'quaking', 'shivering', 'aghast', 'spooked', 'hysterical', 'scared stiff', 'timid', 'alarmed', 'startled', 'spooked', 'shaky', 'tremulous', 'insecure', 'edgy', 'uncomfortable', 'creeped out'],
+  bored: ['bored', 'uninterested', 'indifferent', 'apathetic', 'listless', 'uninspired', 'tedious', 'weary', 'fatigued', 'mundane', 'monotonous', 'lifeless', 'stagnant', 'dull', 'unexciting', 'dreary', 'tiresome', 'lethargic', 'blah', 'unmotivated', 'tedium', 'ennui', 'apathy', 'weariness', 'yawn', 'snooze', 'drowsy', 'sluggish', 'unenthused', 'drab', 'drear', 'wearisome', 'humdrum', 'tiring', 'stale', 'bland', 'unstimulating', 'insipid', 'colorless', 'lackluster', 'unremarkable', 'unvaried', 'mind-numbing', 'stifling', 'stultifying', 'sleepy', 'dragging', 'ho-hum']
+  }
 
 // normal routes ===============================================================
 
@@ -14,9 +27,6 @@ module.exports = function(app, passport, db,) {
     app.get('/videoChat', function(req, res) {
       res.render('video.ejs');
     });
-    
-
-
     // PROFILE SECTION =========================
     app.get('/profile', isLoggedIn, function(req, res) {
         db.collection('journalEntry').find({name: req.user.local.email} ).toArray((err, result) => {
